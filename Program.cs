54602d72cff101bacbd962096e61447e317f2ee9
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-11-24 12:42:27 -0300

    Maior numero nao finalizado

--- Program.cs
+++ Program.cs
@@ -6,26 +6,23 @@ namespace maior_numero
     {
         static void Main(string[] args)
         {
-            int[] n = new int[10];
+            float[] n = new float[10];
+            float maiorvalor = 0, menorvalor = 0, comp = 0;
 
-            int maiorvalor, menorvalor;
-
-            maiorvalor = 0;
-            menorvalor = 0;
             Console.WriteLine("Digite 10 numeros:");
-            for (var i = 0; i < 10; i++)
+            for (var i = 0; i < n.Length; i++)
             {
-                n[i] = int.Parse(Console.ReadLine());
-            }
-            for (var i = 0; i < 10; i++)
-            {
-                if (n[i] > n[0] && n[i] > n[1] && n[i] > n[2] && n[i] > n[3] && n[i] > n[4] && n[i] > n[5] && n[i] > n[6] && n[i] > n[7] && n[i] > n[8] && n[i] > n[9])
-                {
-                   maiorvalor = n[i]; 
-                }
-                else if (n[i] < n[0] && n[i] < n[1] && n[i] < n[2] && n[i] < n[3] && n[i] < n[4] && n[i] < n[5] && n[i] < n[6] && n[i] < n[7] && n[i] < n[8] && n[i] < n[9])
+                Console.WriteLine($"Digite o {i+1}º valor");
+                n[i] = float.Parse(Console.ReadLine());
+
+                comp = n[i];
+                if(n[i] < comp)
                 {
                     menorvalor = n[i];
+                }
+                else if (n[i] > comp)
+                {
+                    maiorvalor = n[i];
                 }
             }
             Console.WriteLine($"O maior valor é {maiorvalor} e o menor valor é {menorvalor}");
